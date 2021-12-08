baf45bd8d5cc202cf8ac07fffb13cc5a2a003e8d
PaulVua <paulvuarambon@gmail.com>
2021-12-08 15:54:18 +0100

    ajout code Arduino mega V0

---
+++ include/logos.h
@@ -0,0 +1,16 @@
+#ifndef LOGOS_H /* include guards */
+#define LOGOS_H
+
+#include <RGBmatrixPanel.h>
+
+extern RGBmatrixPanel matrix;
+
+void LogoAirCarto();
+void LogoAtmoSud();
+void LogoGemenos();
+void LogoModuleAir();
+void AirInterieur();
+void RegionSud();
+void AirExt();
+
+#endif
